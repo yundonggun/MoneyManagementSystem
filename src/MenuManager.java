139eb0f69607dabb026b2365c23c76faca44712f
yundonggun <ehdrjs3311@naver.com>
2022-03-10 11:57:35 +0900

    recover hello.java and added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,11 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Income");
+		System.out.println("2. Subtract Expense");
+		System.out.println("3. Edit Content");
+		System.out.println("4. Show Balance");
+		System.out.println("5. Exit");
+	}
+}
