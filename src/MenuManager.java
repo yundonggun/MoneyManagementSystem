8930b5a5168a412ba329246999f3adf57bb977d6
yundonggun <ehdrjs3311@naver.com>
2022-04-16 02:19:07 +0900

    class project #5

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -6,28 +6,44 @@ public class MenuManager {   										//
 		MoneyManager moneyManager = new MoneyManager(input);		// 메모리할당
 		
 		int num = 0;        				  						// int num 선언 및 0 저장
-		while(num != 5) {											// 입력받은 num 값이 6이 아닐 때 반복문 수행
-			System.out.println("1. Add Income");					// 1-6번까지 메뉴 출력
-			System.out.println("2. Subtract Expense");
+		while(num != 5) {											// 입력받은 num 값이 5이 아닐 때 반복문 수행
+			System.out.println("*** Money Manager ***");
+			System.out.println("1. Add Content");					// 1-5번까지 메뉴 출력
+			System.out.println("2. Delete Content");
 			System.out.println("3. Edit Content");
-			System.out.println("4. Show Balance");
+			System.out.println("4. Show Content");
 			System.out.println("5. Exit");							
-			System.out.println("Select one number between 1-5: ");	// 1-6번 중에서 선택하라는 말 출력
+			System.out.print("Select one number between 1-5: ");	// 1-5번 중에서 선택하라는 말 출력
 			num = input.nextInt();									// 입력받은 숫자를 저장한다 num에 저장한다
 			if(num == 1) {			// 입력 받은 num이 1일 때
-				moneyManager.addincome();		// addincome 메서드로 이동
+				moneyManager.addContent();		// addContent 메서드로 이동
 			}
 			else if(num == 2) {		// 2일 때
-				moneyManager.substract();		// substract 메서드로 이동
+				moneyManager.deleteContent();		// deleteContent 메서드로 이동
 			}
 			else if(num == 3) {		// 3일 때
-				moneyManager.edit();				// edit 메서드로 이동
+				moneyManager.editContent();				// editContent 메서드로 이동
 			}
 			else if(num == 4) {		// 4일 때
-				moneyManager.balance();			// balance 메서드로 이동
+				moneyManager.showContent();			// showContent 메서드로 이동
 			}
 		}
-	} 
-	
-	
+	} 	
 }
+/* 클래스 설명
+1. 기능
+MenuManager 프로그램은 숫자를 입력받아서 그에 따른 계산을 해서 잔액을 보여주는 프로그램이다.
+2. 소스코드 흐름 및 실행
+MenuManager 프로그램에서 처음 메뉴를 출력하고 숫자를 입력 받는다. MoneyManager 클래스에서는 MenuManager 클래스에서 입력 받은 숫자에 해당하는 함수를 
+실행한다. 
+ArrayList를 사용하여 사용자에게 입력 받은 값을 모두 저장한다. 이때 월과 일을 입력 받아서 그에 맞는 곳에 저장한다.
+1을 입력 받았을 때 addContent 메서드로 이동한다. addContent에서는 1부터 3까지 값을 입력받아 값에 해당하는 클래스의 동작을 수행한다. 
+2를 입력 받았을 때 deleteContent 메서드로 이동한다. deleteContent에서는 사용자가 입력한 월과 일이 저장된 값에 있을 때 그에 있는 내용을 삭제하는 기능을 한다.
+3을 입력 받았을 때 editContent 메서드로 이동한다. editContent에서는 사용자가 입력한 월과 일이 저장된 값에 있을 때 거기서 1부터 5까지 숫자를 입력해서 원하는 부분을 
+수정하거나 editContent를 끝낼 수 있다. 4를 입력 받았을 때 printInfo를 호출하여 이때까지 저장한 값을 모두 출력하게 된다. 
+Date 클래스에서는 MoneyManager에 사용되는 필드를 저장한다. printInfo 메서드로 입력 받은 값을 출력하고 getUserInput 메서드로 사용자에게 값을 입력 받는다.
+February 클래슨는 Date 클래스를 상속 받는다. 이때 사용자가 입력하는 day 값이 범위에서 벗어날 경우 다시 입력하라는 문장을 출력하고 제대로 입력되었을 때 다른 값을 입력받고
+저장한다. Month30과 Month31 클래스는 마지막 날이 30일 이거나 31일인 달에 사용한다. 클래스에서는 먼저 달을 입력 받고 나머지는 February 클래스와 동일한 기능을 한다. 
+3. input, output
+input은 int형이고 output은 String형 month1, content, place이고 int형 day, income, expense이다. 
+*/
--- src/MoneyManager.java
+++ src/MoneyManager.java
@@ -1,67 +1,130 @@
 import java.util.Scanner;
+import Money.Date;
+import Money.February;
+import Money.Month30;
+import Money.Month31;
+
+import java.util.ArrayList;
 
 public class MoneyManager {
-	Money day;		// 클래스 레벨에서 day 생성
+	ArrayList<Date> dates = new ArrayList<Date>();		// 클래스 레벨에서 배열 day 생성
 	Scanner input;		// 클래스 내용에 필드에 입력
 	MoneyManager(Scanner input) {
 		this.input = input;	
 	}
 	
-	public void addincome() {
-		day = new Money();				// 기본 생성자 
-		System.out.print("Day: ");		// 날짜 입력
-		day.days = input.nextInt();		// 입력한 내용 저장
-		System.out.print("Income: ");		// 수입을 입력하라는 문장 출력
-		day.income = input.nextInt();		// income에 입력받은 값 할당
-		System.out.print("Content: ");		// 내용을 입력하라는 문장 출력
-		day.content = input.next();			// content에 입력받은 값 할당
-		System.out.print("Place: ");		// 장소를 입력하라는 문장 출력
-		day.place = input.next();		// 문자열 place에 입력받은 값 할당
+	public void addContent() {
+		int month = 0;
+		Date date;
+		while (month != 1 && month != 2 && month != 3) {
+			System.out.print("1 for Months with 30 days, ");
+			System.out.print("2 for Months with 31 days, ");
+			System.out.println("3 for February");
+			System.out.print("Select num for Quater between 1 - 3: ");
+			month = input.nextInt();
+			if(month == 1) {
+				date = new Month30();
+				date.getUserInput(input);
+				dates.add(date);
+				break;
+			}
+			else if(month == 2) {
+				date = new Month31();
+				date.getUserInput(input);
+				dates.add(date);
+				break;
+			}
+			else if(month == 3) {
+				date = new February();
+				date.getUserInput(input);
+				dates.add(date);
+				break;
+			}
+			else {
+				System.out.println("Select num for Quater between 1 - 3");
+			}
+		}
+		
 	}
 	
-	public void substract() {				
-		System.out.print("Days: ");			// 날짜 입력
+	public void deleteContent() {				
+		System.out.print("Month: ");
+		String somemonth = input.next();
+		System.out.print("Day: ");			// 날짜 입력
 		int someday = input.nextInt();		// 날짜 저장
-		if(day == null) {					
-			System.out.println("No data");
+		int index = -1;
+		for(int i = 0; i < dates.size(); i++) {
+			if(dates.get(i).getMonth1().equals(somemonth)) {			// 입력한 날짜와 처음 날짜가 같으면
+				if(dates.get(i).getDay() == someday) {
+					index = i;
+				break;
+				}
+			}
+		}
+		if (index >= 0) {
+			dates.remove(index);
+			System.out.println("The data of " + somemonth + " " + someday + " is deleted");
+		}
+		else {
+			System.out.println("The day has not been register");
 			return;
 		}
-		if(day.days == someday) {			// 입력한 날짜와 처음 날짜가 같으면
-			System.out.print("Expense: ");	// 소비 입력
-			day.expense = input.nextInt();	// 소비 expense에 할당
-			System.out.print("Content: ");	// 내용 입력	
-			day.content = input.next();		// content에 할당
-			System.out.print("Place: ");	// 장소 입력
-			day.place = input.next();		// place에 할당
-		}
 	}
 	
-	public void edit() {
+	public void editContent() {
+		System.out.print("Month: ");
+		String somemonth = input.next();
 		System.out.print("Days: ");
 		int someday = input.nextInt();
-		if(day == null) {
-			System.out.println("No data");
-		}
-		if(day.days == someday) {
-			System.out.print("Content: ");		// 내용 입력하라는 문장 출력
-			String edit1 = input.next();			// edit 문자열에 입력받은 값 할당
-			day.content = edit1;				// content에 edit 할당
-			System.out.print("Place: ");		// 장소 입력하라는 문장 출력
-			String edit2 = input.next();		// edit2에 입력받은 값 할당
-			day.place = edit2;				// place에 edit2 할당
-		}	
+		for(int i = 0; i < dates.size(); i++) {
+			Date date = dates.get(i);
+			if(date.getMonth1().equals(somemonth)) {
+				if(date.getDay() == someday) {
+					int num = -1;
+					while(num!= 5) {
+						System.out.println("*** Money Manager ***");
+						System.out.println("1. Edit Income");	
+						System.out.println("2. Edit Expense");
+						System.out.println("3. Edit Content");
+						System.out.println("4. Edit Place");
+						System.out.println("5. Exit");		
+						System.out.println("Select oen number between 1 - 5");
+						num = input.nextInt();
+						if (num == 1) {
+							System.out.print("Income: ");
+							int income = input.nextInt();
+							date.setIncome(income);
+						}
+						else if (num == 2) {
+							System.out.print("Expense: ");
+							int expense = input.nextInt();
+							date.setExpense(expense);
+						}
+						else if (num == 3) {
+							System.out.print("Content: ");
+							String content = input.next();
+							date.setContent(content);
+						}
+						else if (num == 4) {
+							System.out.print("Place: ");
+							String place = input.next();
+							date.setPlace(place);
+						}
+						else {
+							continue;
+						}
+					} // while
+				} // if
+			} // if
+			else {
+				System.out.println("No Data");
+			} // else
+		} // for
 	}
 	
-	public void balance() {
-		System.out.print("Days: ");
-		int someday = input.nextInt();
-		if(day == null) {
-			System.out.println("No data");
-		}
-		if(day.days == someday) {			// 입력한 내용이 날짜에 있을 때
-			day.balance = day.income - day.expense;		// 잔액을 계산하는 식
-			System.out.printf("Days: %d Content: %s Place: %s\n", day.days, day.content, day.place);  // 날짜와 내용과 장소 출력
-			System.out.printf("Balance: %d\n", day.balance); 	// 잔액을 출력
+	public void showContent() {
+		for(int i = 0; i < dates.size(); i++) {
+			dates.get(i).printInfo();
 		}
 	}
-} 
+}
